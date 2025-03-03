08a255f4ccbcf524647230121fa10318bcf277fd
afsalprimamaula <afsalprimamaula@gmail.com>
2025-03-03 15:58:22 +0700

    menyelesaikan tugas3

---
+++ kuliah/pertemuan2/pertemuan4/latihan1.php
@@ -0,0 +1,19 @@
+<?php
+// pengulangan / loop / looping
+
+$i = 5;
+while ($i >= 1) {
+    echo "hello world! $i x <br>";
+    $i -= 1;
+}
+
+// for
+// for (nilai_awal; kondisi_terminasi; increment/dec) {
+// aksi
+// }
+echo "<hr>";
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "hello world! $i x <br>";
+}
+// semua kasus pakai FOR
---
+++ kuliah/pertemuan2/pertemuan4/latihan3.php
@@ -0,0 +1,34 @@
+<?php
+$i = 1;
+// looping i untuk total baris
+// variabel J untuk Angka
+for ($i = 1; $i <= 2; $i++) {
+    for ($j = 1; $j <= 6; $j++) {
+        echo "$j ";
+    }
+    echo "<br>";
+}
+
+echo "<hr>";
+
+$i = 1;
+// looping i untuk total baris
+// variabel J untuk Angka
+for ($i = 1; $i <= 7; $i++) {
+    for ($j = 1; $j <= 2; $j++) {
+        echo "$j ";
+    }
+    echo "<br>";
+}
+
+echo "<hr>";
+
+$i = 1;
+// looping i untuk total baris
+// variabel J untuk Angka
+for ($i = 1; $i <= 10; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "$i ";
+    }
+    echo "<br>";
+}
